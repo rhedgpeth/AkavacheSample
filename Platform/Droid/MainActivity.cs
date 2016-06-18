c9568998d3bc3f247783da686e67de44d0f8ff7a
Robert Hedgpeth <robert.hedgpeth@xamarin.com>
2016-06-18 07:53:32 -0500

    Let's try that again..

---
+++ Platform/Droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using System;
+using System.Threading.Tasks;
+
+using AkavachePCLSample.Services;
+
+namespace AkavachePCLSample.Droid
+{
+	[Activity(Label = "AkavachePCLSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += GetSecretStuffs;
+		}
+
+		void GetSecretStuffs(object sender, EventArgs e)
+		{
+			Task.Run(async () =>
+			{
+				var secretThangs = await ExampleService.Instance.GetSecretness();
+
+				if (secretThangs != null)
+				{
+					// Do something with the results!
+				}
+			});
+		}
+	}
+}
+
+
---
+++ Platform/iOS/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using UIKit;
+
+using AkavachePCLSample.Services;
+
+namespace AkavachePCLSample.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{ }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			Button.AccessibilityIdentifier = "myButton";
+
+			Button.TouchUpInside += GetSecretStuffs;
+		}
+
+		void GetSecretStuffs(object sender, EventArgs e)
+		{
+			Task.Run(async () =>
+			{
+				var secretThangs = await ExampleService.Instance.GetSecretness();
+
+				if (secretThangs != null)
+				{
+					// Do something with the results!
+				}
+			});
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.		
+		}
+	}
+}
